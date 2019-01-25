2993fc7d22f44f0c096f410cccdc7524a07bda60
guer <guer@guer.com>
2019-01-25 11:07:10 +0800

    weather.db

---
+++ app/src/main/java/com/example/administrator/weather/db/County.java
@@ -0,0 +1,45 @@
+package com.example.administrator.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+
+    private String countyName;
+
+    private int weatherId;
+
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
